79486ea3c4a97ad7ad7262942b5a702284f51888
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-19 22:55:20 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,7 @@
+void main(){
+    System.out.println("Rakesh" +" kumar");
+    System.out.print("Singh");
+    System.out.println("Kushwaha");
+    System.out.print("Ji");
+    // Hi this is my name
+}
