6a4d77790664c42f68ddb29767de4053f3c96338
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-19 22:55:06 +0530

    Add files via upload

---
+++ AssignOP.java
@@ -0,0 +1,18 @@
+public class AssignOP {
+    static void main(){
+        int a=5;
+        System.out.println(a);
+
+        a+=7;
+        System.out.println(a);
+
+        a-=7;
+        System.out.println(a);
+
+        a*=8;
+        System.out.println(a);
+
+        a/=7;
+        System.out.println(a);
+    }
+}
