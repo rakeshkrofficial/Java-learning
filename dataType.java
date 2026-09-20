fe92658d9fc785e60e86b21d0658ce7477012f19
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-20 23:18:27 +0530

    Add files via upload

---
+++ dataType.java
@@ -0,0 +1,29 @@
+static void main(){
+    byte num=127;
+
+    System.out.println(num);
+
+    long numb=num;
+    System.out.println("Long num:"+num);
+
+    short num2=32767;
+
+    System.out.println(num2);
+     int num3=50000;
+     long num4=324921378;
+     System.out.println(num3);
+     System.out.println(num4);
+
+     float num5=3.1416232f;
+     System.out.println(num5);
+
+     double num6=3.142487628476864786735673587264;
+     System.out.println(num6);
+
+     boolean aligibleVote=true;
+     System.out.print(aligibleVote);
+
+     char first_Charecter='a';
+
+     System.out.println("Next charecter is : " + (char)(first_Charecter+1));
+}
