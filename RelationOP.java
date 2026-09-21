d288cfa0a88b62ca02c8d93698105ca09f75177c
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-21 23:25:44 +0530

    Add files via upload

---
+++ RelationOP.java
@@ -0,0 +1,12 @@
+public class RelationOP {
+    static void main(){
+        int a=5;
+        int b=10;
+
+        System.out.println(a==b);
+        System.out.println(a !=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a<b);
+    }
+}
