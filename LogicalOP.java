c010d785eff5607c16b799a31a7771fd6278aeef
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-21 23:24:07 +0530

    Add files via upload

---
+++ LogicalOP.java
@@ -0,0 +1,11 @@
+public class LogicalOP {
+    static void main(){
+        boolean completedDsa=true;
+        boolean compleCore = false;
+
+        System.out.println(completedDsa && compleCore);
+        System.out.println(compleCore || completedDsa);
+        System.out.println( ! completedDsa);
+
+    }
+}
