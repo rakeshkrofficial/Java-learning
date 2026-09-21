273aed06bed18aecfebf9228ab1a0932ec93464e
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-21 23:24:55 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,14 @@
+// Write a program to
+
+class Math
+{
+    public static void main(String args[])
+    {
+        int a = 5;
+        int b = 7;
+        System.out.println("a+b ="+ (a+b));
+        System.out.println("b-a ="+ (b-a));
+        System.out.println("a*b ="+ (a*b));
+        System.out.println("b/a =" + (a/b));
+    }
+}
