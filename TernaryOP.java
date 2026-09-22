f406e409ab3c81cae450910e00c2a82927549c71
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-22 22:57:09 +0530

    Add files via upload

---
+++ TernaryOP.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TernaryOP {
+    static void main(){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter Your marks:");
+//        int marks=sc.nextInt();
+//
+//        String status = (marks >=50) ? "Pass" : "Fail" ;
+//
+//        System.out.print(status);
+
+        int age =27;
+        String ans =(age>=18) ? "Exprience person" :"Child" ;
+
+        System.out.print("You are a " + ans);
+    }
+}
